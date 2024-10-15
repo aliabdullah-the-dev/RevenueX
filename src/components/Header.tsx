4d507c74ac618a6910f479dbc34697392b79677d
Ali Abdulla <the.ch.alabdullah@gmail.com>
2024-10-15 23:43:54 +0500

    initial Commit after Project setup with pnpm

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function Header() {
+  return (
+    <header className="flex">
+        <div className=""></div>
+      <Button>Click me</Button>
+    </header>
+  )
+}
+
+export default Header;
